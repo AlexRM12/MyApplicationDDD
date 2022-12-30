4d6efbccb2496659575302f5421fb41d4a63aad8
Alberto Hidalgo <foliolo7891@gmail.com>
2022-12-30 17:30:36 +0000

    GPS refactor

--- app/src/main/java/com/example/myapplicationd/FirstFragment.kt
+++ app/src/main/java/com/example/myapplicationd/FirstFragment.kt
@@ -1,146 +1,36 @@
 package com.example.myapplicationd
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.pm.PackageManager
-import android.location.*
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
-import com.example.myapplicationd.databinding.FragmentFirstBinding
-
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+import com.example.myapplicationd.databinding.FragmentBinding
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class FirstFragment : Fragment() ,LocationListener {
+class FirstFragment : Fragment() {
 
+    private lateinit var binding: FragmentBinding
 
-    private val CODIGO_PERMISOS_UBICACION_SEGUNDO_PLANO = 2106
-    private var haConcedidoPermisos = false
-    private var _binding: FragmentFirstBinding? = null
-    private lateinit var locationManager: LocationManager
-    private val locationPermissionCode = 2
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentBinding.inflate(inflater, container, false)
         return binding.root
-
     }
-    @RequiresApi(Build.VERSION_CODES.P)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener() {
-            getLocation()
-        }
-    }
-    @RequiresApi(Build.VERSION_CODES.P)
-    private fun getLocation() {
-        verificarPermisos()
-        locationManager = (requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager)!!
-        if ((ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-
-            ) != PackageManager.PERMISSION_GRANTED)
-        ) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                locationPermissionCode
-            )
-        }
-        val isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-
-        if (locationManager!!.getAllProviders().contains("network")) {
-            if (isGPSEnabled) {
-                locationManager!!.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER,
-                    5000,
-                    5f,
-                    this
-
-                )
-                //   locationManager.getCurrentLocation(
-                //       LocationManager.GPS_PROVIDER,requireContext().mainExecutor,
-                Log.i(
-                    "",
-                    "locationManager!!.getAllProviders().contains()"
-                )
-
-
-            } else {
-                Log.i("", "requestLocationUpdates fet")
+        binding.fragmentButton.apply {
+            text = getString(R.string.next)
+            setOnClickListener {
+                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
             }
-        } else {
-            Log.i("", "requestLocationUpdates error")
-
         }
+
+        binding.progressIndicator.isVisible = false
     }
-
-
-    private fun verificarPermisos() {
-        val permisos = arrayListOf(
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-
-            )
-        val permisosComoArray = permisos.toTypedArray()
-        if (tienePermisos(permisosComoArray)) {
-            haConcedidoPermisos = true
-            // onPermisosConcedidos()
-            //   Log.d(Chart.LOG_TAG, "Los permisos ya fueron concedidos")
-        } else {
-            solicitarPermisos(permisosComoArray)
-        }
-    }
-
-    private fun solicitarPermisos(permisos: Array<String>) {
-        // Log.d(Chart.LOG_TAG, "Solicitando permisos...")
-        requestPermissions(
-            permisos,
-            CODIGO_PERMISOS_UBICACION_SEGUNDO_PLANO
-        )
-    }
-
-    private fun tienePermisos(permisos: Array<String>): Boolean {
-        return permisos.all {
-            return ContextCompat.checkSelfPermission(
-                requireActivity(),
-                it
-            ) == PackageManager.PERMISSION_GRANTED
-        }
-    }
-
-    @SuppressLint("SetTextI18n")
-    override fun onLocationChanged(location: Location) {
-    binding.textviewFirst.setText(
-        location.latitude.toString() +"\n"+
-        location.longitude.toString())
-    }
-
-    override fun onProviderEnabled(provider: String) {}
-
-    override fun onProviderDisabled(provider: String) {}
-
-    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
-
 }
-
--- app/src/main/java/com/example/myapplicationd/SecondFragment.kt
+++ app/src/main/java/com/example/myapplicationd/SecondFragment.kt
@@ -1,44 +1,124 @@
 package com.example.myapplicationd
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.myapplicationd.databinding.FragmentSecondBinding
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.myapplicationd.databinding.FragmentBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices.getFusedLocationProviderClient
+import com.google.android.gms.location.Priority
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class SecondFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentBinding
+    private lateinit var locationManager: LocationManager
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
-
+    @SuppressLint("SetTextI18n")
+    private val locationListener = LocationListener { location ->
+        binding.progressIndicator.isVisible = false
+        binding.fragmentText.text = """
+          Lat: ${location.latitude}
+          Lng: ${location.longitude}
+     """.trimIndent()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+    @RequiresApi(Build.VERSION_CODES.N)
+    val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                // Precise location access granted.
+                Log.i(TAG, "Precise location access granted")
+                startLocationUpdates()
+            }
+
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                Log.i(TAG, "Only approximate location access granted")
+                binding.progressIndicator.isVisible = false
+            }
+
+            else -> {
+                // No location access granted.
+                Log.w(TAG, "Permissions not granted")
+                binding.progressIndicator.isVisible = false
+            }
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        val locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY, 10000
+        ).build()
+
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest, locationListener, Looper.getMainLooper()
+        )
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fusedLocationClient = getFusedLocationProviderClient(requireActivity())
+
+        binding.fragmentButton.apply {
+            text = getString(R.string.previous)
+            setOnClickListener() {
+                findNavController().navigateUp()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onStart() {
+        super.onStart()
+
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationListener)
+    }
+
+    companion object {
+        private const val TAG = "SecondFragment"
     }
 }
+
